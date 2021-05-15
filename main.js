280033ef021c79686d2786a2182c5dc562de5ca1
boriswart <boriswart@yahoo.com>
2021-05-15 12:01:19 -0600

    got containners fixed

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let productionUpgrades = [{
     name: "10 Kw Toaster",
     desc: "2 toastes per click",
     cost: 100,
-    img: "assets/img/toaster.png",
+    img: "assets/img/5k-toaster.png",
     func: "upgradeNumToast",
     passive: false,
     enabled: false
@@ -17,7 +17,7 @@ let productionUpgrades = [{
     name: "Flame Thrower",
     desc: "5 toasts per click",
     cost: 500,
-    img: "assets/img/flamer.gif",
+    img: "assets/img/flame-throw-small.png",
     func: "upgradeFlameToast",
     passive: false,
     enabled: false
@@ -26,7 +26,7 @@ let productionUpgrades = [{
     name: "Robot Toaster",
     desc: "5 toasts per period",
     cost: 1000,
-    img: "assets/img/gears-robit.gif",
+    img: "assets/img/robot-toaster.png",
     func: "upgradeRobotToast",
     passive: true,
     enabled: false
@@ -35,7 +35,7 @@ let productionUpgrades = [{
     name: "2nd Robot Toaster",
     desc: "period reduced to 3s",
     cost: 2000,
-    img: "assets/img/speed-upgrade.gif",
+    img: "assets/img/robot-toaster2.png",
     func: "upgradeIncRobotToast",
     passive: true,
     enabled: false
@@ -44,16 +44,16 @@ let productionUpgrades = [{
     name: "3rd Robot Toaster",
     desc: "period reduced to 3s",
     cost: 2000,
-    img: "assets/img/speed-upgrade.gif",
+    img: "assets/img/robot-toaster3.png",
     func: "upgradeIncRobotToast",
     passive: true,
     enabled: false
 },
 {
     name: "4th Robot Toaster",
-    desc: "period reduced to 3s",
+    desc: "period to to 3s",
     cost: 2000,
-    img: "assets/img/speed-upgrade.gif",
+    img: "assets/img/robot-toaster4.png",
     func: "upgradeIncRobotToast",
     passive: true,
     enabled: false
@@ -69,8 +69,8 @@ function pickToast() {
 function drawScreen() {
     collectedToastElem.innerText = `Toast Collected: ${collectedToast.toString()}`
     toastVaultElem.innerText = `Toast Vault: ${toastVault.toString()}`
-    drawUpgrades('left', 3, 0)  //draws left side starting from 0 to 3 of productionUpgrades
-    drawUpgrades('right', 6, 3) //draws right side starting from 3 to 6 of productionUpgrades
+    drawUpgrades("left", 3, 0)  //draws left side starting from 0 to 3 of productionUpgrades
+    drawUpgrades("right", 3, 3)  //draws right side starting from 3 to 6 of productionUpgrades
 }
 
 function drawUpgrades(side, count, starting) {
@@ -88,7 +88,7 @@ function drawUpgrades(side, count, starting) {
                 <div>
                     <p class="no-margin"> ${productionUpgrades[x].name}</p>
                     <p class="no-margin"> ${productionUpgrades[x].desc}</p>
-                    <p class="no-margin"> ${productionUpgrades[x].cost}</p>
+                    <p class="no-margin"> cost: ${productionUpgrades[x].cost}</p>
                 </div>
             </div>
         </div>
@@ -97,10 +97,9 @@ function drawUpgrades(side, count, starting) {
     upgradeElem.innerHTML = template
 }
 
-function updateNumToast() {
+function upgradeNumToast() {
     if (numToastPerClick < 2) numToastPerClick = 2
     //TODO    disable future numToastPerClick 
     console.log("numToastPerClick updated to two!", numToastPerClick)
 }
-
 drawScreen()
